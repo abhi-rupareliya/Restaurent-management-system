bb51d2dd18defa6999eae246af2fcdb24cc1cc73
Ankit Savani <ankitmsavani12345@gmail.com>
2023-02-28 19:05:40 +0530

    Validate Form Fields
    Signed-off-by: Ankit Savani <ankitmsavani12345@gmail.com>

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -10,9 +10,30 @@ const Login = () => {
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
     const navigate = useNavigate()
+    const [errors, setErrors] = useState({})
 
     const Submitted = async (e) => {
         e.preventDefault()
+
+        e.preventDefault()
+
+        // Validate form fields
+        const errors = {}
+        if (!Email) {
+            errors.email = 'Email is required'
+        } else if (!/\S+@\S+\.\S+/.test(Email)) {
+            errors.email = 'Email is invalid'
+        }
+        if (!Password) {
+            errors.password = 'Password is required'
+        } else if (Password.length < 8) {
+            errors.password = 'Password must be at least 8 characters'
+        }
+        if (Object.keys(errors).length > 0) {
+            setErrors(errors)
+            return
+        }
+
         const res = await FLogin(Email, Password)
 
         if (res.success) {
@@ -37,26 +58,17 @@ const Login = () => {
                         <form className="space-y-4 md:space-y-6" onSubmit={Submitted}>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                                <input type="email" name="email" onChange={
-                                    (e) => {
-                                        e.preventDefault()
-                                        setEmail(e.target.value)
-                                    }
-                                } className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="" />
+                                <input type="email" name="email" value={Email} onChange={(e) => setEmail(e.target.value)} className={`bg-gray-50 border ${errors.email ? 'border-red-500' : 'border-gray-300'} text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`} placeholder="name@company.com" required />
+                                {errors.email && <p className="text-sm text-red-500">{errors.email}</p>}
                             </div>
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                <input type="password" name="password" onChange={
-                                    (e) => {
-                                        e.preventDefault()
-                                        setPassword(e.target.value)
-                                    }
-                                }  placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" value={Password} onChange={(e) => setPassword(e.target.value)} className={`bg-gray-50 border ${errors.password ? 'border-red-500' : 'border-gray-300'} text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`} placeholder="••••••••" required />
+                                {errors.password && <p className="text-sm text-red-500">{errors.password}</p>}
                             </div>
-                            
-                            <button type="submit" className="w-full text-white bg-orange-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
+                            <button type="submit" className="w-full text-white bg-orange-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                             <p className="text-sm font-light text-gray-500">
-                                Don’t have an account yet? <Link to='/signup' className="font-medium text-primary-600 hover:underline">Sign up</Link>
+                                Don't have an account yet? <Link to='/signup' className="font-medium text-primary-600 hover:underline">Sign up</Link>
                             </p>
                         </form>
                     </div>
--- front-end/src/Components/Signup.js
+++ front-end/src/Components/Signup.js
@@ -8,9 +8,10 @@ const Signup = () => {
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
     const [Role, setRole] = useState("")
+    const [errors, setErrors] = useState({});
     const navigate = useNavigate()
     return (
-            <section className="Signup bg-[url('/public/images/bglogin.jpg')] bg-cover">
+        <section className="Signup bg-[url('/public/images/bglogin.jpg')] bg-cover">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
                 <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                     <Link to='/home' className="flex items-center text-2xl font-semibold text-gray-900 justify-center ">
@@ -24,7 +25,14 @@ const Signup = () => {
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
                                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Username</label>
-                                <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" onChange={(e) => { setUserName(e.target.value) }} value={userName}  placeholder="First & Last name" required="" />
+                                <input type="username" name="username" id="username" className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.userName ? "border-red-500" : ""}`}
+                                    onChange={(e) => setUserName(e.target.value)}
+                                    value={userName} placeholder="Enter Username :" required=""
+                                />
+                                {errors.userName && (
+                                    <p className="text-red-500 text-sm mt-1">{errors.userName}</p>
+                                )}
+
                             </div>
                             <div>
                                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">What's your work ?</label>
@@ -32,27 +40,68 @@ const Signup = () => {
                                     {Role === "" ? <option value="">Select Role</option> : null}
                                     <option value="cashier">Cashier</option>
                                     <option value="manager">Manager</option>
-                                </select>                                
+                                </select>
                             </div>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" onChange={(e) => { setEmail(e.target.value) }} value={Email} placeholder="name@company.com" required="" />
+                                <input type="email" name="email" id="email" className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ${errors.email ? "border-red-500" : ""}`}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={Email} placeholder="name@company.com" required=""
+                                />
+                                {errors.email && (
+                                    <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+                                )}
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" onChange={(e) => { setPassword(e.target.value) }} value={Password} required="" />
+                                <input type="password" name="password" value={Password} onChange={(e) => setPassword(e.target.value)} className={`bg-gray-50 border ${errors.password ? 'border-red-500' : 'border-gray-300'} text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`} placeholder="••••••••" required />
+                                {errors.password && <p className="text-sm text-red-500">{errors.password}</p>}
                             </div>
                             <button onClick={async (e) => {
                                 e.preventDefault()
-                                if (userName === "" || Email === "" || Password === "" || Role === "") {
-                                    alert("Enter All Field")
+                                const errors = {};
+                                //validations
+                                if (!userName.trim()) {
+                                    errors.userName = "Username is required";
+                                } else if (userName.length < 4) {
+                                    errors.userName = "Username must be at least 4 characters";
                                 }
-                                else {
-                                    const res = await FSignup(Email, Password, userName, Role)
-                                    localStorage.setItem("myToken", res.authtoken)
-                                    navigate('/home')
-                                    console.warn(res)
-                                }}} type="submit" className="w-full text-white bg-orange-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign up</button>
+
+                                if (!Email.trim()) {
+                                    errors.email = "Email is required";
+                                } else if (!/\S+@\S+\.\S+/.test(Email)) {
+                                    errors.email = "Invalid email address";
+                                }
+
+                                if (!Password.trim()) {
+                                    errors.password = "Password is required";
+                                } else if (Password.length < 8) {
+                                    errors.password = "Password must be at least 8 characters";
+                                } else if (/\s/.test(Password)) {
+                                    errors.password = "Password cannot contain whitespace";
+                                } else if (!/[A-Z]/.test(Password)) {
+                                    errors.password = "Password must contain at least one uppercase letter";
+                                } else if (!/[a-z]/.test(Password)) {
+                                    errors.password = "Password must contain at least one lowercase letter";
+                                } else if (!/\d/.test(Password)) {
+                                    errors.password = "Password must contain at least one number";
+                                } else if (!/[!@#$%^&_*]/.test(Password)) {
+                                    errors.password = "Password must contain at least one special character";
+                                }
+                                if (!Role.trim()) {
+                                    errors.role = "Role is required";
+                                }
+
+                                if (Object.keys(errors).length === 0) {
+                                    // fetching the result
+                                    const res = await FSignup(Email, Password, userName, Role);
+                                    localStorage.setItem("myToken", res.authtoken);
+                                    navigate("/home");
+                                    console.warn(res);
+                                } else {
+                                    setErrors(errors);
+                                }
+                            }} type="submit" className="w-full text-white bg-orange-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign up</button>
                             <p className="text-sm font-light text-gray-500">
                                 You already have an account? <Link to='/login' className="font-medium text-primary-600 hover:underline">Login</Link>
                             </p>
