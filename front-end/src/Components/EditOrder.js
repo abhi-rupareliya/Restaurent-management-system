4a7a2a2a6f4577b8ba947b6d3eaf9295014d6ea2
Ankit Savani <ankitmsavani12345@gmail.com>
2023-03-30 21:46:09 +0530

    Design Dropdown and minor fixes
    Signed-off-by: Ankit Savani <ankitmsavani12345@gmail.com>

--- front-end/src/Components/EditOrder.js
+++ front-end/src/Components/EditOrder.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Fragment, useRef } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Transition, Menu } from "@headlessui/react";
 import Navbar from './Navbar';
 import SideBar from './SidebarCSR';
 import { useParams } from 'react-router-dom';
 import FGetMenu from '../Function/F_GetMenu';
 import FGetOrders from '../Function/F_GetOrders';
 import FUpdateOrders, { FAddOrder } from '../Function/F_UpdateOrders';
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
 const EditOrder = () => {
     const id = useParams().tab_id;
     const [openDelete, setOpenDelete] = useState(false)
     const cancelButtonRef = useRef(null)
     const [items, setItems] = useState([]);
-    const [selectedItem, setSelectedItem] = useState('');
+    const [selectedItem, setSelectedItem] = useState('Select an Item');
     const [options, setOptions] = useState([]);
     const [qty, setQty] = useState(1)
 
@@ -37,11 +43,11 @@ const EditOrder = () => {
 
     const handleAddItem = async () => {
         try {
-            const itemExists = items.some((item) => item.item._id === selectedItem);
+            const itemExists = items.some((item) => item.item._id === selectedItem._id);
 
             if (itemExists) {
                 const updatedItems = items.map((item) => {
-                    if (item.item._id === selectedItem) {
+                    if (item.item._id === selectedItem._id) {
                         return {
                             ...item,
                             quantity: parseInt(item.quantity) + parseInt(qty),
@@ -52,7 +58,7 @@ const EditOrder = () => {
                 await FUpdateOrders(id, updatedItems);
                 fetchData();
             } else {
-                await FAddOrder(id, selectedItem, qty)
+                await FAddOrder(id, selectedItem._id, qty)
                 fetchData();
             }
         } catch (error) {
@@ -68,22 +74,71 @@ const EditOrder = () => {
             <SideBar />
             <div className="bg-[url('/public/images/dashbackground.jpg')]">
                 <div className="bg-black/75 col-span-5 p-4 sm:ml-64 backdrop-blur-sm  min-h-screen">
-                    <div className="p-4 border rounded-lg md:space-x-8 md:mt-0  md:font-medium md:border-0 text-[#f06826] text-2xl">
-                        <div className=" flex justify-center px-4 pt-5 pb-4 sm:p-6 sm:pb-4 p-0">
+                    <div className="border rounded-lg md:space-x-8 md:mt-0  md:font-medium md:border-0 text-[#f06826] text-2xl">
+                        <div className=" flex justify-center px-4 pb-4 sm:p-6 sm:pb-4 p-0">
                             <div className="sm:flex sm:items-start w-full">
                                 <div className="mt-3 text-center sm:mt-0 sm:m-4 sm:text-left w-full">
-                                    <select className='bg-transparent' value={selectedItem} onChange={handleSelect}>
-                                        <option value="">Select an item</option>
-                                        {options.map((option) => (
-                                            <option key={option._id} value={option._id}>
-                                                {option.item_name}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <input className='bg-transparent' value={qty} onChange={(e) => {
-                                        setQty(e.target.value);
-                                    }} />
-                                    <button onClick={handleAddItem}>Add</button>
+                                    <div className="m-3 flex justify-between">
+                                        <div className=" border rounded-lg md:flex-row md:space-x-8 md:mt-0  md:font-medium md:border-0 text-[#f06826] text-2xl">
+                                            {/* <select className='bg-transparent' value={selectedItem} onChange={handleSelect}> */}
+                                            <Menu as="div" className="relative inline-block text-left">
+                                                <div>
+                                                    <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 md:font-medium  rounded-md text-[#f06826] bg-transparent px-3 py-2 font-semiboldshadow-sm">
+                                                        {!selectedItem.item_name ? "Select an Item" : selectedItem.item_name}
+                                                        <ChevronDownIcon className="-mr-1 h-5 w-5 mt-2 text-[#f06826]" aria-hidden="true" />
+                                                    </Menu.Button>
+                                                </div>
+
+                                                <Transition
+                                                    as={Fragment}
+                                                    enter="transition ease-out duration-100"
+                                                    enterFrom="transform opacity-0 scale-95"
+                                                    enterTo="transform opacity-100 scale-100"
+                                                    leave="transition ease-in duration-75"
+                                                    leaveFrom="transform opacity-100 scale-100"
+                                                    leaveTo="transform opacity-0 scale-95"
+                                                >
+                                                    <Menu.Items className="absolute z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                        <div className="py-1" >
+                                                        {options.map((option) => (
+                                                            <Menu.Item>
+                                                                {({ active }) => (
+                                                                    <a 
+                                                                        href="#"
+                                                                        onClick={() => {
+                                                                            setSelectedItem(option);
+                                                                        }}
+                                                                        className={classNames(
+                                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                            'block px-4 py-2 text-sm'
+                                                                        )} 
+                                                                    >
+                                                                        {option.item_name}
+                                                                    </a>
+                                                                )}
+                                                            </Menu.Item>
+                                                              ))}
+                                                        </div>
+                                                    </Menu.Items>
+                                                </Transition>
+                                            </Menu>
+                                        </div>
+                                        <div className='border px-3 py-2 rounded-lg md:flex-row md:space-x-8 md:mt-0  md:font-medium md:border-0 text-[#f06826] text-2xl'>
+                                            Quantity:
+                                            <input className='bg-transparent mx-3' value={qty} onChange={(e) => {
+                                                setQty(e.target.value);
+                                            }} />
+                                        </div>
+                                        <div className="md:w-auto place-self-center" >
+                                            <ul className=" border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+                                                <li>
+                                                    <button className="text-white bg-[#f26926] hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2 text-center" onClick={handleAddItem}>
+                                                        Add</button>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
                                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-0">
                                         <table className="w-full text-sm text-left text-gray-400" >
                                             <thead className="text-xs text-[#f26926] uppercase bg-[#2d2d2d] l">
@@ -165,18 +220,18 @@ const EditOrder = () => {
                                                                                     fetchData()
                                                                                 }}
                                                                             >
-                                                                                
+
                                                                                 <span className="m-auto text-2xl font-thin">+</span>
                                                                             </button>
-                                                                            
+
                                                                         </div>
                                                                     </td>
                                                                     <td className='px-6 py-6'>
-                                                                    <button className="font-medium text-[#f26926] hover:underline" onClick={async (e) => {
-                                                                                    // e.preventDefault()
-                                                                                    setOpenDelete(true)
-                                                                                    // setIid(itm.item._id)
-                                                                                }} >Remove</button>
+                                                                        <button className="font-medium text-[#f26926] hover:underline" onClick={async (e) => {
+                                                                            // e.preventDefault()
+                                                                            setOpenDelete(true)
+                                                                            // setIid(itm.item._id)
+                                                                        }} >Remove</button>
                                                                     </td>
                                                                 </tr>
                                                             </>
@@ -223,7 +278,7 @@ const EditOrder = () => {
                                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                 >
                                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 content-center " >
+                                        <div className="bg-white px-4 pb-4 sm:p-6 sm:pb-4 content-center " >
                                             <div className="flex justify-center">
                                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                                     <Dialog.Title
