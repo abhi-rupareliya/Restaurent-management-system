5bc7015fdd002c7838c6110c02bfbce7c8ea3e75
JAIVIN <jpsavaliya14@gmail.com>
2023-03-31 11:03:38 +0530

    Order Routes
    Signed-off-by: JAIVIN <jpsavaliya14@gmail.com>

--- Backend/Routes/OrderRoutes.js
+++ Backend/Routes/OrderRoutes.js
@@ -1,19 +1,42 @@
-const path = require('path')
-const Table = require('../Models/Order')
-function OrderRoutes(app)
-{
-    app.get('/',async(req,res) => {
+const Orders = require('../Models/Orders')
+const Table = require('../Models/Table')
+const mongoose = require('mongoose')
+module.exports = (app) => {
+    app.post('/SaveOrder/:tabid', async (req, res) => {
+        const table = req.params.tabid;
+
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
         try {
-            
+            const ods = await Table.findOne({ tab_id: table })
+                .populate({ path: "orders.item", select: ["item_name", "item_price"] });
 
+            const newOrder = new Orders({
+                table: table,
+                orders: ods.orders,
+                date_time: new Date()
+            });
 
+            const savedOrder = await newOrder.save({ session });
 
+            await Table.findOneAndUpdate(
+                { tab_id: table },
+                { $set: { orders: [] } },
+                { session, new: true }
+            );
 
+            await session.commitTransaction();
+            session.endSession();
 
-            // res.status(200).send({ success: true, order })
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('error');
+            res.status(200).send({ savedOrder });
+        } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+
+            console.error(error);
+            res.status(500).send({ message: 'An error occurred while saving the order.' });
         }
-    })
+    });
+
 }
